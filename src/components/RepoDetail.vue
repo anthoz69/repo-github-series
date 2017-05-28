21a94f80740b65c6f59914b61284a4f00c594c3a
Stp.Develo <stp.develo@gmail.com>
2017-05-28 19:45:18 +0700

    - สร้างหน้า สำหรับแสดงรายชื่อคน - สร้างหน้าแบบ List สำหรับ Reuse

--- src/components/RepoDetail.vue
+++ src/components/RepoDetail.vue
@@ -1,8 +1,33 @@
 <template>
-  <div></div>
+  <section class="section">
+    <h1 class="title is-1 has-text-centered">{{ repoName }}</h1>
+    <div class="columns is-multiline">
+      <div class="column is-3" v-if="listSubscribers" v-for="user in listSubscribers">
+        <RepoList :user="user" :key="user.id"></RepoList>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import { fetchGitHubSubscribers } from '@/api/'
+import RepoList from './RepoList'
+
 export default {
+  components: {
+    RepoList
+  },
+  data () {
+    return {
+      repoName: this.$route.params.username,
+      listSubscribers: null
+    }
+  },
+  created () {
+    fetchGitHubSubscribers(this.repoName).then((response) => {
+      console.log(response)
+      this.listSubscribers = response.data
+    })
+  }
 }
 </script>
---
+++ src/components/RepoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="user.avatar_url" alt="Image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="is-4">{{ user.login }}</p>
+          <a :href="user.html_url" target="_bank">@{{ user.login }}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['user']
+}
+</script>
