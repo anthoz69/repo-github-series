6a19b8b2edec78e7260ba128cfd226842c287dcf
Stp.Develo <stp.develo@gmail.com>
2017-05-28 20:48:04 +0700

    - อัพเดทไฟล์ API - Create instance axios for reuse

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,22 @@
 import axios from 'axios'
 
-export function fetchGitHubRepo (path) {
-  let result = axios.get(`https://api.github.com/repos/${path}`, {
-    headers: {
-      Authorization: `token YORU_API_TOKEN`
-    }
-  })
+const API = axios.create({
+  headers: {
+    Authorization: `token YORU_API_TOKEN`
+  }
+})
+
+function fetchGitHubRepo (path) {
+  let result = API.get(`https://api.github.com/repos/${path}`)
   return result
+}
+
+function fetchGitHubSubscribers (path) {
+  let result = API.get(`https://api.github.com/repos/${path}/subscribers`)
+  return result
+}
+
+export {
+  fetchGitHubRepo,
+  fetchGitHubSubscribers
 }
