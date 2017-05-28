2a065fc6a16760dd9d1a3e376eca30afde3be8ea
Stp.Develo <stp.develo@gmail.com>
2017-05-28 19:07:32 +0700

    - ลบ Token

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function fetchGitHubRepo (path) {
   let result = axios.get(`https://api.github.com/repos/${path}`, {
     headers: {
-      Authorization: `token 93a6403eebb8400814573969e79db6c8994ee707`
+      Authorization: `token YORU_API_TOKEN`
     }
   })
   return result
